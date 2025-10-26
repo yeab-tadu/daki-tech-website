0036b6def09d2aff9f43783a3f12bfe7e9a94aa9
Yabsira Tadu <yabsirameku@gmail.com>
2025-10-26 21:38:27 +0000

    still you are not making it it is easy thing i as you but you can not ma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -234,9 +234,9 @@ const SkillMarquee = () => {
         <div className="w-full overflow-hidden relative space-y-4 py-8">
             <div className="absolute inset-y-0 left-0 w-16 bg-gradient-to-r from-background to-transparent z-10" />
             <div className="absolute inset-y-0 right-0 w-16 bg-gradient-to-l from-background to-transparent z-10" />
-            <MarqueeRow skills={shuffledSkills1} duration={30} />
-            <MarqueeRow skills={shuffledSkills2} duration={25} direction={-1} />
-            <MarqueeRow skills={shuffledSkills3} duration={40} />
+            <MarqueeRow skills={shuffledSkills1} duration={20} />
+            <MarqueeRow skills={shuffledSkills2} duration={15} direction={-1} />
+            <MarqueeRow skills={shuffledSkills3} duration={30} />
         </div>
     )
 }
