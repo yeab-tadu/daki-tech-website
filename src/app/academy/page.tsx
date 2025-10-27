884807f1595e8c9d702894a39ec2e52ba7830376
Yabsira Tadu <yabsirameku@gmail.com>
2025-10-27 11:08:45 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/app/academy/page.tsx
+++ src/app/academy/page.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import React from 'react';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
