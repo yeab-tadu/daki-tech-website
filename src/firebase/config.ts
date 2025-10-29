55f9901472a5e52811a2ac36a145ac25bf0dec01
Yabsira Tadu <yabsirameku@gmail.com>
2025-10-29 11:45:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5652691263-b3196",
+  "appId": "1:555653747407:web:fce237facb3f10df3e1dba",
+  "apiKey": "AIzaSyC18Aya7gMswE6V8j7YEGuqbkg4ar5_mmw",
+  "authDomain": "studio-5652691263-b3196.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "555653747407"
+};
